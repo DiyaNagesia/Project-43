b85cba44ba59153190d9af4fb9478671902d73a8
DiyaNagesia <73646449+DiyaNagesia@users.noreply.github.com>
2021-08-30 13:34:41 +0100

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "FINGERPRINTS";
+const accessCode2 = "DOZENS";
+const accessCode3 = "AUTOMOBILE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("P N G F E S N R I R I T ", 100,50)
+    fill("lightblue")
+    text("Hint: The more you take,the more you leave behind.Who am I?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N O D E S Z", 700,150)
+    fill("lightblue")
+    text("Hint: A word I know, six letters it contains,removes one letter,and twelve remains.Who am I?", 700,170,250,80)
+
+    fill("white")
+    textSize(15)
+    text("B U M O O T L E I A", 100,250)
+    fill("lightblue")
+    text("Hint: What has ten letters and starts with gas?", 100,270)
+
+}
